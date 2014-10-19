8044af06e90820d3b7d48fc8d0d072ce7bffea31
Martin Malmsten <martin.malmsten@gmail.com>
2014-10-19 23:02:19 +0200

    Version 0.0.1

--- sobs.py
+++ sobs.py
@@ -1,43 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import (Flask, render_template, request, make_response, Response, abort, send_file, redirect, url_for)
+# SOBS - Simple OBject Storage
+
+from flask import (Flask, render_template, request, make_response, Response, abort, send_file, redirect, url_for, safe_join)
 from argparse import ArgumentParser
 from uuid import uuid4
 from os.path import abspath,dirname,basename,join,isfile,isdir
 from os import makedirs
 from hashlib import md5
+from datetime import datetime
 
 app = Flask(__name__)
-app.debug = True
+#app.debug = True
 ROOT=None
+TMP=None
 
 @app.route('/store', methods=[ 'PUT' ])
 def store():
-    if 'uri' in request.args and 'path' in request.args:
-        abort(412)
-
-    uri = request.args.get('uri', uuid4().urn)
-    path = request.args.get('path', get_path(uri))
+    uri = request.args.get('uri', None)
+    path = request.args.get('path', None)
     hash = request.args.get('hash', None)
 
-    archive(path, request.stream, expected_hash=hash)
+    if uri and path:
+        return Response('Parameters uri and path mutually exclusive', status=412)
+    elif not uri or path:
+        uri = uuid4().urn
 
-    return uri
+    if path:
+        if '..' in path:
+            return Response('\'..\' not allowed in path', 412)
+
+        path = safe_join(ROOT, 'path/' + path)
+    else:
+        path = safe_join(ROOT, 'hash/' + get_path(uri))
+
+    archive(path, request.stream, uri=uri, expected_hash=hash)
+
+    return 'OK'
+
 
 @app.route('/retrieve', methods=[ 'GET' ])
 def retrieve():
-    path = get_path(request.args.get('uri'))
+    uri = request.args.get('uri', None)
+    path = request.args.get('path', None)
+    hash = request.args.get('hash', None)
+
+    if uri and path:
+        return Response('Parameters uri and path mutually exclusive\n', status=412)
+    
+    if path:
+        if '..' in path:
+            return Response('\'..\' not allowed in path', 412)
+
+        path = safe_join(ROOT, 'path/' + path)
+    else:
+        path = safe_join(ROOT, 'hash/' + get_path(uri))
 
     if not isfile(path):
-        abort(404)
+        return Response('No such file\n', status=412)
 
     return send_file(path)
 
-def archive(uri, stream, expected_hash=None):
-    path = get_path(uri)
+
+def archive(path, stream, uri=None, expected_hash=None):
     data = ' '
     hash = md5()
+
+    # actually DO checksum checking
 
     if not isdir(dirname(path)):
         makedirs(dirname(path))
@@ -48,31 +78,32 @@ def archive(uri, stream, expected_hash=None):
             hash.update(data)
             f.write(data)
 
-def get_path(uri):
-    if uri.startswith('urn:uuid'):
-        s = [ 'urn', 'uuid' ] + [ uri[9:][ 2*i:2*i+2 ] for i in range(0,4) ] + [ uri ]
-    elif uri.startswith('urn:nbn'):
-        hex = md5(uri).hexdigest()
-        s = [ 'urn', 'nbn' ] + [ hex[ 2*i:2*i+2 ] for i in range(0,4) ] + [ hex ]
-    elif uri.startswith('http') or uri.startswith('https'):
-        s = [ x for x in split(':|/|-', uri) if x != '' ]
+    if uri:
+        _log('STORED object with uri %s at %s, md5:%s' % (uri, path, hash.hexdigest()))
     else:
-        hex = md5(uri).hexdigest()
-        s = [ 'md5' ] + [ hex[ 2*i:2*i+2 ] for i in range(0,4) ] + [ hex ] 
+        _log('STORED object at %s, md5:%s' % (path, hash.hexdigest()))
+
+
+def get_path(uri):
+    hex = md5(uri).hexdigest()
+    s = [ hex[ 2*i:2*i+2 ] for i in range(0,4) ] + [ hex ]
     
-    return '/'.join([ ROOT ] + s)
+    return '/'.join(s)
+
 
 def _log(message, t=datetime.utcnow()):
-    with open("%s-log" % self.url[7:], 'a') as logfile:
+    with open("%s/log" % ROOT, 'a') as logfile:
         logfile.write(t.isoformat() + ' ' + message + '\n')
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    #parser.add_argument('-p', '--path_scheme', default='hash')
+    parser.add_argument('-t', '--temp_directory', default='/tmp')
     parser.add_argument('-p', '--port', type=int, default=8080)
     parser.add_argument('archive')
     args = vars(parser.parse_args())
     ROOT = abspath(args['archive'])
+    TMP=abspath(args['temp_directory'])
 
     app.run(host='0.0.0.0', port=args['port'], threaded=True)
 
